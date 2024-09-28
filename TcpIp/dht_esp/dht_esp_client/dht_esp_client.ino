e8fceaf47e7025b03219cde24eed574ae6e0c03f
ydam9182 <69256285+ydam9182@users.noreply.github.com>
2024-09-29 01:51:40 +0900

    Add files via upload
    dht_esp_client
    - remove esp description

---
+++ TcpIp/dht_esp/dht_esp_client/dht_esp_client.ino
@@ -0,0 +1,113 @@
+//온습도 센서 + esp -> strtok해보기
+#include <WiFiEsp.h>
+#include <SoftwareSerial.h>
+#include <SimpleDHT.h>
+
+// 소프트웨어 직렬 포트를 통해 ESP8266과 통신
+SoftwareSerial Serial1(10, 11); // RX, TX
+
+// Wi-Fi 네트워크 정보
+char ssid[] = "PYD";        // Wi-Fi 네트워크 이름
+char pass[] = "gksksla12@";    // Wi-Fi 비밀번호
+
+// 리눅스 채팅 서버 정보
+char server_ip[] = "192.168.1.75";  // 리눅스 서버 IP 주소
+int server_port = 9100;              // 리눅스 서버 포트 번호
+
+WiFiEspClient client;  // Wi-Fi 클라이언트 객체
+
+// 온습도센서 핀, 객체선언, 보낼 메세지
+#define DHT_PIN 2
+SimpleDHT11 dht11;
+char msg[7] = {0};
+
+// 받아온 서버메세지에 사용할 버퍼와 토큰
+char serverMsg[100] = {0};
+int serv_buf = 0;
+char *pToken;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(9600);  // ESP8266과의 소프트웨어 직렬 통신 시작
+
+  // ESP8266 초기화
+  WiFi.init(&Serial1);
+  
+  // Wi-Fi 연결 확인
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi 모듈을 찾을 수 없습니다.");
+    while (true);
+  }
+
+  // Wi-Fi 네트워크에 연결
+  connectToWiFi();
+
+  // 리눅스 채팅 서버에 연결
+  if (client.connect(server_ip, server_port)) {
+    Serial.println("서버에 성공적으로 연결되었습니다.");
+    client.println("Yedam의 ESP8266 채팅 클라이언트");  // 서버에 첫 메시지 전송
+  } else {
+    Serial.println("서버에 연결할 수 없습니다.");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  byte temp, humi;
+  int err = SimpleDHTErrSuccess;
+
+  dht11.read(DHT_PIN, &humi, &temp, NULL);
+  sprintf(msg, "%d:%dL", (int)humi, (int)temp);
+
+  Serial.print("보낸 값 : ");
+  client.write(msg);
+  Serial.println(msg);
+  delay(2000);
+
+  if(Serial1.available()){
+    char c; //하나씩 따와서 저장
+
+    while(Serial1.available()) {
+      c = Serial1.read();
+      serverMsg[serv_buf++]= c; //버퍼 저장
+    }
+    serverMsg[serv_buf] = '\0'; //마지막은 닫아주고
+
+    pToken = strtok(serverMsg, ":"); // 콜론 기점으로 앞에 필요없는값 버리고
+    pToken = strtok(NULL, "*"); //*기점으로 으로 실제 메세지 저장하기
+
+    Serial.print("받은 메세지 : ");
+    Serial.println(pToken);
+    serv_buf = 0;
+  }
+
+  // 서버와의 연결이 끊어졌는지 확인
+  if (!client.connected()) {
+    Serial.println("서버와의 연결이 끊어졌습니다.");
+    client.stop();
+    while (true);  // 연결이 끊어지면 멈춤
+  }
+}
+
+
+// Wi-Fi 네트워크에 연결하는 함수
+void connectToWiFi() {
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Wi-Fi에 연결 중... ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
+    delay(5000);  // 연결 대기
+  }
+  Serial.println("Wi-Fi에 연결되었습니다.");
+  printWifiStatus();
+}
+
+// Wi-Fi 상태를 출력하는 함수
+void printWifiStatus() {
+  // IP 주소 출력
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP 주소: ");
+  Serial.println(ip);
+}
+
